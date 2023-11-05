07ea3a8fea4f8aa4688c7dea4646fe14ea2e0e95
Rostik Natkha <106112250+rostiq@users.noreply.github.com>
2023-11-05 13:02:00 +0200

    change name

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import App from 'components/App';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="goit-react-hw-08-phonebook">
+      <BrowserRouter basename="phonebook">
       <PersistGate loading={null} persistor={persistor}>
         <App />
         </PersistGate>
